b3b621e3da4371ba65a757c41447a6e3d2a42d31
Rivaldo Roberto <rrs.sistema@gmail.com>
2024-04-02 01:25:23 -0300

    Ajustes gerais

--- lista-tarefa/src/assets/js/app.js
+++ lista-tarefa/src/assets/js/app.js
@@ -1,3 +1,4 @@
+let counter = 0;
 
 function adicionarTarefa() {
     const novaTarefa = document.getElementById('input_nova_tarefa').value;
@@ -5,15 +6,53 @@ function adicionarTarefa() {
 };
 
 function criaNovaTarefa(textoTarefa) {
-    const listaTarefas = document.getElementById('lista_de_tarefas');
-    let quantidade = listaTarefas.children.length;
-    const novoItem = document.createElement('li');
-    novoItem.innerText = textoTarefa;
-    novoItem.id = `tarefa_id_${quantidade}`;
-    novoItem.appendChild(criaInputCheckBoxTarefa(novoItem.id));
+    const table = document.getElementById("table");
+    let quantidade = table.children.length;
+    const novaTarefa = document.createElement('td');
+    novaTarefa.innerText = textoTarefa;
+    const idTarefa = `tarefa_id_${quantidade}`;
+    novaTarefa.id = idTarefa;
+    novaTarefa.appendChild(criaInputCheckBoxTarefa(idTarefa));
 
-    listaTarefas.appendChild(novoItem);
-    console.log(`Tarefa adicionada: ${textoTarefa}`);
+    // Create a new row
+    var row = document.createElement("tr");
+    // Create two new cells
+    var cellTextoTarefa = document.createElement("td");
+    cellTextoTarefa.id = idTarefa;
+    cellTextoTarefa.innerHTML = textoTarefa;
+
+    var cellCheckBox = document.createElement("td");
+    var cellEditar = document.createElement("td");
+    var cellDeletar = document.createElement("td");
+    const rowId = `row_id_${quantidade}`;
+
+    cellCheckBox.appendChild(criaInputCheckBoxTarefa(idTarefa));
+    cellEditar.appendChild(criaInputButtonEditarTarefa(idTarefa));
+    cellDeletar.appendChild(criaInputButtonDeletarTarefa(rowId));
+
+    // Adiciona as celulas na linha atual
+    row.id = rowId;
+    row.appendChild(cellTextoTarefa);
+    row.appendChild(cellCheckBox);
+    row.appendChild(cellEditar);
+    row.appendChild(cellDeletar);
+    row.style
+
+    // Append the row to the table
+    table.appendChild(row);// Adiciona a linha na tabela
+    const inputNovaTarefa = document.getElementById('input_nova_tarefa');
+    inputNovaTarefa.value = ''; // Limpa o input de tarefa
+    document.getElementById("input_nova_tarefa").focus(); // Seta o foco no input
+
+    var linhasTabela = document.getElementsByTagName("tr");
+    for (var i = 0; i < linhasTabela.length; i++) {
+        if ((i) % 2 == 0)
+            linhasTabela[i + 1].style.background = "#A9A9A9";
+        else
+            linhasTabela[i + 1].style.background = "lightgrey";
+    }
+
+    counter++;
 }
 
 function criaInputCheckBoxTarefa(idTarefa) {
@@ -23,6 +62,26 @@ function criaInputCheckBoxTarefa(idTarefa) {
     return inputTarefa;
 }
 
+function criaInputButtonEditarTarefa(idTarefa) {
+    var btnEdit = document.createElement('input');
+    btnEdit.id = "buttonEditar";
+    btnEdit.type = "button";
+    btnEdit.value = 'Editar';
+    btnEdit.className = "btn_generic btn_editar";
+    btnEdit.setAttribute('onclick', `alterarTarefa('${idTarefa}')`);
+    return btnEdit;
+}
+
+function criaInputButtonDeletarTarefa(rowId) {
+    var btnDelet = document.createElement('input');
+    btnDelet.id = "buttonDeletar";
+    btnDelet.type = "button";
+    btnDelet.value = 'Deletar';
+    btnDelet.className = "btn_generic btn_deletar";
+    btnDelet.setAttribute('onclick', `deletarTarefa('${rowId}')`);
+    return btnDelet;
+}
+
 function mudaEstadoTarefa(idTarefa) {
     const tarefaSelecionada = document.getElementById(idTarefa);
     if (tarefaSelecionada.style.textDecoration == 'line-through') {
@@ -30,6 +89,22 @@ function mudaEstadoTarefa(idTarefa) {
     } else {
         tarefaSelecionada.style = 'text-decoration: line-through;'
     }
+}
+
+function alterarTarefa(idTarefa) {
+    const tarefaSelecionada = document.getElementById(idTarefa);
+    if (tarefaSelecionada.style.textDecoration == 'line-through') {
+        tarefaSelecionada.style = 'text-decoration: none;'
+    } else {
+        tarefaSelecionada.style = 'text-decoration: line-through;'
+    }
+}
+
+function deletarTarefa(rowId) {
+    var row = document.createElement("tr");
+    var row = document.getElementById(rowId);
+    row.parentNode.removeChild(row);
+    alert(`Deletando tarefa: ${rowId}`);
 }
 
 function adicionaListaTarefaLocalStorage() {

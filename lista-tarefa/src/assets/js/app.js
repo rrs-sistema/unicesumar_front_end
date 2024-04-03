57161f4c6d4d3fd60848d7160ed72c33130ad36a
Rivaldo Roberto <rrs.sistema@gmail.com>
2024-04-03 00:47:21 -0300

    Ajustes gerais

--- lista-tarefa/src/assets/js/app.js
+++ lista-tarefa/src/assets/js/app.js
@@ -1,36 +1,65 @@
 const localStorage = window.localStorage;
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
-localStorage.setItem('items', JSON.stringify(itemsArray))
-const data = JSON.parse(localStorage.getItem('items'))
+let tarefaArrayLista = localStorage.getItem('listTarefas') ? JSON.parse(localStorage.getItem('listTarefas')) : [];
+localStorage.setItem('listTarefas', JSON.stringify(tarefaArrayLista));
+const dadosLocalStorage = JSON.parse(localStorage.getItem('listTarefas'));
 
-data.forEach(item => {
-    criaNovaTarefa(item, false)
-});
+
+class TarefaObj {
+    constructor(id, descricao, status) {
+        this.id = id;
+        this.descricao = descricao;
+        this.status = status;
+    }
+}
+
+
+
+window.onload = function () {
+    dadosLocalStorage.forEach(item => {
+        criaNovaTarefa(item, false)
+    });
+    console.log('EVENTO ONLOAD ACONTECENDO...');
+};
 
 function adicionarTarefa() {
     const novaTarefa = document.getElementById('input_nova_tarefa').value;
-    criaNovaTarefa(novaTarefa, true);
-};
-
-function criaNovaTarefa(textoTarefa, vemCadastrado) {
-    let temItem = false;
-    console.log('Verificando item');
-    for (var i = 0; i < itemsArray.length; i++) {
-        if (itemsArray[i].trim() == textoTarefa.trim()) {
-            temItem = true;
-            break;
-        }
-    }
-    if (!temItem) {
-        itemsArray.push(textoTarefa);
-    } else if (temItem && vemCadastrado) {
-        alert('OPS!, Essa tarefa já foi cadastrada.');
+    if (novaTarefa == null || novaTarefa == '') {
+        alert('OPS!, Innforme a descrição da tarefa.');
         return;
     }
     const table = document.getElementById("table");
     let quantidade = table.children.length;
+    let objTarefa = new TarefaObj();
+    objTarefa.id = quantidade;
+    objTarefa.descricao = novaTarefa;
+    objTarefa.status = false;
+    criaNovaTarefa(objTarefa, true);
+};
+
+function criaNovaTarefa(objTar, vemCadastrado) {
+    let temItem = false;
+
+    for (var i = 0; i < tarefaArrayLista.length; i++) {
+        if (tarefaArrayLista[i].descricao.trim() == objTar.descricao.trim()) {
+            temItem = true;
+            break;
+        }
+    }
+    const table = document.getElementById("table");
+    let quantidade = table.children.length;
+    if (!temItem) {
+        let objTarefa = new TarefaObj();
+        objTarefa.id = quantidade;
+        objTarefa.descricao = objTar.descricao;
+        objTarefa.status = false;
+        tarefaArrayLista.push(objTarefa);
+    } else if (temItem && vemCadastrado) {
+        alert('OPS!, Essa tarefa já foi cadastrada.');
+        return;
+    }
+
     const novaTarefa = document.createElement('td');
-    novaTarefa.innerText = textoTarefa;
+    novaTarefa.innerText = objTar.descricao;
     const idTarefa = `tarefa_id_${quantidade}`;
     novaTarefa.id = idTarefa;
     novaTarefa.appendChild(criaInputCheckBoxTarefa(idTarefa));
@@ -40,7 +69,7 @@ function criaNovaTarefa(textoTarefa, vemCadastrado) {
     // Create two new cells
     var cellTextoTarefa = document.createElement("td");
     cellTextoTarefa.id = idTarefa;
-    cellTextoTarefa.innerHTML = textoTarefa;
+    cellTextoTarefa.innerHTML = objTar.descricao;
 
     var cellCheckBox = document.createElement("td");
     var cellEditar = document.createElement("td");
@@ -57,13 +86,24 @@ function criaNovaTarefa(textoTarefa, vemCadastrado) {
     row.appendChild(cellCheckBox);
     row.appendChild(cellEditar);
     row.appendChild(cellDeletar);
-    row.style
+
+
 
     // Append the row to the table
     table.appendChild(row);// Adiciona a linha na tabela
     const inputNovaTarefa = document.getElementById('input_nova_tarefa');
     inputNovaTarefa.value = ''; // Limpa o input de tarefa
     document.getElementById("input_nova_tarefa").focus(); // Seta o foco no input
+
+    const idInputChecked = `checkbox_${idTarefa}`;
+
+    if (!objTar.status) {
+        row.style = 'text-decoration: none;';
+        document.getElementById(idInputChecked).checked = false;
+    } else {
+        row.style = 'text-decoration: line-through;';
+        document.getElementById(idInputChecked).checked = true;
+    }
 
     var linhasTabela = document.getElementsByTagName("tr");
     for (var i = 0; i < linhasTabela.length; i++) {
@@ -76,11 +116,12 @@ function criaNovaTarefa(textoTarefa, vemCadastrado) {
         }
     }
 
-    localStorage.setItem('items', JSON.stringify(itemsArray))
+    localStorage.setItem('listTarefas', JSON.stringify(tarefaArrayLista))
 }
 
 function criaInputCheckBoxTarefa(idTarefa) {
     const inputTarefa = document.createElement('input');
+    inputTarefa.id = `checkbox_${idTarefa}`;
     inputTarefa.type = 'checkbox';
     inputTarefa.setAttribute('onclick', `mudaEstadoTarefa('${idTarefa}')`);
     return inputTarefa;
@@ -97,32 +138,87 @@ function criaButtonGeneric(nome, className, funcao) {
 }
 
 function mudaEstadoTarefa(idTarefa) {
-    const tarefaSelecionada = document.getElementById(idTarefa)
-    if (tarefaSelecionada.style.textDecoration == 'line-through') {
-        tarefaSelecionada.style = 'text-decoration: none;'
-    } else {
-        tarefaSelecionada.style = 'text-decoration: line-through;'
+    const tarefa = document.getElementById(idTarefa).innerHTML;
+    row_id_3
+
+    let indexItem = -1;
+    let objTarefa = new TarefaObj();
+    objTarefa.id = indexItem;
+    objTarefa.descricao = tarefa;
+
+    for (var i = 0; i < tarefaArrayLista.length; i++) {
+        if (tarefaArrayLista[i].descricao.trim() == tarefa.trim()) {
+            objTarefa.status = !tarefaArrayLista[i].status;
+            indexItem = i;
+            break;
+        } else {
+            objTarefa.status = tarefaArrayLista[i].status;
+        }
     }
+    let idRowSelected = idTarefa.replace('tarefa_id_', 'row_id_');
+    const rowSelected = document.getElementById(idRowSelected);
+
+    if (!objTarefa.status) {
+        rowSelected.style = 'text-decoration: none;';
+    } else {
+        rowSelected.style = 'text-decoration: line-through;';
+    }
+
+    tarefaArrayLista.splice(indexItem, 1);
+    tarefaArrayLista.push(objTarefa);
+    localStorage.setItem('listTarefas', JSON.stringify(tarefaArrayLista));
 }
 
 function alterarTarefa(idTarefa) {
     const tarefaSelecionada = document.getElementById(idTarefa).innerHTML;
-    let tarefaAlterada = prompt('Alterando tarefa', tarefaSelecionada);
-    if (tarefaAlterada != null) {
-        document.getElementById(idTarefa).innerHTML = tarefaAlterada;
-        let indexItem = -1;
-        for (var i = 0; i < itemsArray.length; i++) {
-            if (itemsArray[i].trim() == tarefaAlterada.trim()) {
-                temItem = true;
-                indexItem = i;
-                break;
+    //let tarefaAlterada = prompt('Alterando tarefa', tarefaSelecionada);
+    let tarefaAlterada = '';
+    let indexItem = idTarefa.replace('tarefa_id_', '');
+    const inputValue = tarefaSelecionada;
+    // Exemplo: https://sweetalert2.github.io/#examples
+    swal({
+        title: 'Alterando tarefa!',
+        type: 'input',
+        inputValue,
+        showCancelButton: true,
+        closeOnConfirm: false,
+        animation: "slide-from-top",
+        confirmButtonText: 'Alterar',
+        cancelButtonText: 'Cancelar',
+        height: 250
+    },
+        function (inputValue) {
+            if (inputValue === false || inputValue === '') {
+                alert('OPS! Por favor informe a tarefa');
+                return false;
             }
-        }
-        itemsArray.splice(indexItem, 1);
-        itemsArray.push(tarefaAlterada);
-        localStorage.setItem('items', JSON.stringify(itemsArray));
-    }
+            tarefaAlterada = inputValue;
+            document.getElementById(idTarefa).innerHTML = inputValue;
+            let status = false;
 
+            if (tarefaAlterada != null) {
+                document.getElementById(idTarefa).innerHTML = tarefaAlterada;
+                for (var i = 0; i < tarefaArrayLista.length; i++) {
+                    status = tarefaArrayLista[i].status;
+                    if (tarefaArrayLista[i].descricao.trim() == tarefaAlterada.trim()) {
+                        temItem = true;
+                        break;
+                    }
+                }
+
+                indexItem = (indexItem - 1);
+
+                let objTarefa = new TarefaObj();
+                objTarefa.id = indexItem;
+                objTarefa.descricao = tarefaAlterada;
+                objTarefa.status = status;
+                tarefaArrayLista.splice(indexItem, 1);
+                tarefaArrayLista.push(objTarefa);
+                localStorage.setItem('listTarefas', JSON.stringify(tarefaArrayLista));
+            }
+
+            swal.close();
+        });
 }
 
 function deletarTarefa(rowId) {
@@ -131,26 +227,10 @@ function deletarTarefa(rowId) {
     if (confirm("Deseja conformar essa ação?")) {
         const id = rowId.replace("row_id_", '')
         row.parentNode.removeChild(row);
-        itemsArray.splice(id - 1, 1);
-        localStorage.setItem('items', JSON.stringify(itemsArray));
+        tarefaArrayLista.splice(id - 1, 1);
+        localStorage.setItem('listTarefas', JSON.stringify(tarefaArrayLista));
     }
     else {
         console.log('Ação abortada tarefa');
-    }
-
-}
-
-
-function adicionaListaTarefaLocalStorage() {
-    const localStorage = window.localStorage;
-    let tarefas = localStorage.getItem('lista_tarefas');
-    console.log(`Metodo onload adicionaListaTarefaLocalStorage: ${tarefas}`);
-    if (tarefas) {
-        // implementar a logica de salvar cada tarefa aqui
-        localStorage.setItem('lista_tarefas', tarefas)
-    } else {
-        // se existir tarefas, então le do localStorage
-        // exemplo de leitura localStorage.getItem('lista_tarefas')
-        // monta no HTML
     }
 }
